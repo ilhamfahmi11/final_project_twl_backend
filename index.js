b2a5cb030e7fbaba8f223e1a2a2529338e5e2afb
ilhamfahmi11 <ilham.fahmi011@gmail.com>
2023-07-24 15:28:09 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import UserRoute from "./routes/UserRoute.js";
+
+const app = express();
+mongoose.connect('mongodb+srv://ilhamfahmi011:if111202@cluster0.9sxx3vm.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Database Connected...'));
+
+app.use(cors());
+app.use(express.json());
+app.use(UserRoute);
+
+app.listen(5000, ()=> console.log('Server up and running...'));
